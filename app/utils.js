6467a0ba822beedbc2c27ea871374d42653867ef
Dan Vingo <danvingo@gmail.com>
2015-07-23 13:34:19 -0400

    Change file path

--- app/utils.js
+++ app/utils.js
@@ -39,8 +39,8 @@ var dataUriToBlob = (dataUri) => {
 
 export default {
 
-  iconPath: (name) => `/${srcDir}/images/icons/${name}`,
-  surfacePath: (name) => `/${srcDir}/images/surfaces/${name}`,
+  iconPath: (name) => `${srcDir}/images/icons/${name}`,
+  surfacePath: (name) => `${srcDir}/images/surfaces/${name}`,
   toA: toA,
   svgLayerIds: svgLayerIds,
 
