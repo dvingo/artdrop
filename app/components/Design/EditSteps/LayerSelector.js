bdece1a14ff3df20a18e0347154c2344c9b42efc
Dan Vingo <danvingo@gmail.com>
2015-07-24 18:13:47 -0400

    Style updates

--- app/components/Design/EditSteps/LayerSelector.js
+++ app/components/Design/EditSteps/LayerSelector.js
@@ -8,10 +8,12 @@ var imgSize = 60
 
 export default React.createClass({
   mixins: [reactor.ReactMixin, Navigation, State],
+
   getDataBindings() {
     return {currentLayerId: ['currentLayerId'],
             design: Store.getters.currentDesign}
   },
+
   selectLayer(layerId) {
     Store.actions.selectLayerId(layerId)
     var imagesOrColors = this.getParams().imagesOrColors
@@ -21,14 +23,17 @@ export default React.createClass({
   },
 
   editSurface() {
+    Store.actions.selectLayerId('')
     this.transitionTo('designEdit', {designId: this.state.design.get('id'), step: 'surface'})
   },
 
   render() {
+    var editingSurface = this.getParams().step === 'surface'
+      console.log('SURAF: ',editingSurface)
     return (
       <article className="layer-selector-wrapper small">
         <div className="container">
-          {this.state.design.get('layers').map(layer => {
+          {this.state.design.get('layers').reverse().map(layer => {
             return (
               <div className="layer-selector"
                    onClick={this.selectLayer.bind(null, layer.get('id'))}>
@@ -40,6 +45,7 @@ export default React.createClass({
           }
           <div className="layer-selector surface">
             <img src={imageUrlForSurface(this.state.design.get('surface'))}
+                 className={classNames({selected: editingSurface})}
                  onClick={this.editSurface}
                  width={imgSize} height={imgSize}/>
           </div>
--- app/components/Nav.js
+++ app/components/Nav.js
@@ -1,22 +1,46 @@
 import React from 'react';
 import {Link} from 'react-router';
 import {iconPath} from '../utils';
+import Router from 'react-router'
 
 export default React.createClass({
+  mixins: [Router.State],
+
+  handleGoBack(e) {
+    e.preventDefault()
+    var haveHistory = window.history.back()
+  },
+
   render() {
+    var onDesignEdit = (this.isActive('designEdit') || this.isActive('layerEdit'))
+
     return (
       <div className="nav-bar">
+
         <Link to="designs" className="logo">
-          <img src={iconPath('logo.png')}/>
+          <img src={iconPath('logo-drop.png')} height={20} width={20}/>
         </Link>
-        <ul className="right-icons">
-          <li>
-            <img src={iconPath('share-icon.svg')}/>
-          </li>
-          <li>
-            <img src={iconPath('cart-icon.svg')}/>
-          </li>
-        </ul>
+
+        {onDesignEdit ? (
+          <img src={iconPath('left-arrow.svg')}
+            height={30} width={30}
+            style={{margin: '0 20px 0 5px'}}
+            onClick={this.handleGoBack}/> ) : null}
+
+        {onDesignEdit ? (
+           <span className="btn">Reset</span>) : null}
+
+        <div className="right-icons">
+          <div className="icon" style={{marginRight:20}}>
+            <img src={iconPath('share-icon.svg')} height={20} width={20}
+                 style={{marginTop:4}}/>
+          </div>
+          {onDesignEdit ? (
+             <span className="price">$75</span>) : null}
+          {onDesignEdit ? (
+             <span className="btn">Buy</span>) : null}
+        </div>
+
       </div>
     )
   }
