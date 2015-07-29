f0020e49a0aa57036620dac5d5f0c951a099a576
Dan Vingo <danvingo@gmail.com>
2015-07-29 14:52:05 -0400

    Test s3

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ module.exports = {
   env: 'prod',
   srcDir: 'app',
   s3Endpoint: 'https://s3.amazonaws.com',
-  //s3BucketName: 'com.artdrop.images',
-  s3BucketName: 'test-dvingo-1',
+  s3BucketName: 'com.artdrop.images',
+  //s3BucketName: 'test-dvingo-1',
   devPort: 8111,
   devHostname: 'localhost',
   prodHostname: 'obscure-atoll-2694.herokuapp.com'
