c2ef4b5b466a999b60f41d631af4753c7cabf220
Dan Vingo <danvingo@gmail.com>
2015-08-03 15:07:27 -0400

    Updates

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -
 
 rm hosted-dir/bundle.js
+export NODE_ENV=production
 echo 'webpack --config webpack.prod.config.js --progress'
 webpack --config webpack.prod.config.js --progress
+export NODE_ENV=dev
