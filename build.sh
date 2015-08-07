acb3b5d76100088a4e3d9bae5502dfbf7b1e2356
Dan Vingo <danvingo@gmail.com>
2015-08-06 22:00:47 -0400

    Fix build

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 rm hosted-dir/bundle.js
 export NODE_ENV=production
+
 echo 'rsync -a app/images/icons/ hosted-dir/app/images/icons'
 rsync -a app/images/icons/ hosted-dir/app/images/icons
+
 echo 'webpack --config webpack.prod.config.js --progress'
 webpack --config webpack.prod.config.js --progress
 export NODE_ENV=dev
