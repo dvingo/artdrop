67f4ab89c37274e0c6ff1a351ba9111e6dee4568
Dan Vingo <danvingo@gmail.com>
2015-09-04 17:07:35 -0400

    Parse variants

--- print-io-api/index.js
+++ print-io-api/index.js
@@ -1,6 +1,7 @@
 var Service = require('./print-io-api')
 var async = require('async')
 var newId = require('./utils').generateFirebaseID
+var margin = 1.4
 
 if (process.argv.length === 2) {
   console.log('You must provide a recipe ID as the only argument.')
@@ -18,7 +19,7 @@ function getProductVariants(p) {
 }
 
 function getPrintingPrice(v) {
-  return v.PriceInfo.Price * 100
+  return Math.round(v.PriceInfo.Price * 100)
 }
 
 function getSalePrice(v) {
@@ -76,6 +77,46 @@ var optionsMap = {
    framedCanvasSize:      '26b66c145877439ab7eb765ad5a52309',
    framedCanvasColor:     '7702d54940094ce5bbb625a816e6a56d',
    framedCanvasThickness: '7e0232ca5e604a18bbb35d192952f1a6',
+   framedPrintMatSize:    '0d408d36ab9d4da5aa7d338f3bf0a6f1',
+   framedPrintColor:      'af66458cf1b844ad9daed9546a6e2216',
+   framedPrintSize:       '3e9da285a0a84ea59633207e0feff5fe',
+   framedPrintType:       'c02deea045fb4d47873985b333db9321',
+   gicleePrintPaperType:  '0d14875084904097a8d5f69133081692',
+   gicleePrintSize:       '2745aa0a75694c599809ea402185954d',
+   hoodieColor:           '50326f0998634f78b51c5e7f68f2dde3',
+   hoodieSize:            'd0ad1ca109d248e09e24f9347f1d4ac8',
+   hoodieBrand:           'c366f11045444bdc93dcf55d5ccc5a3d',
+   hoodieGender:          '9c6a6a75bfb749e6b1fb7e40d76f8948',
+   hoodiePrintLocation:   '3f7e8f484fd946aeb8a6af4cc0e54e7a',
+   photoBookPageCount:    'bf1f9d742519451191e1ef2698727869',
+   photoBookCoverType:    'c42898b2bc5b41728af0c85f5a99dacc',
+   photoBookPaperType:    '3763b6a1e30540e99b466d844b32d381',
+   photoBookPaperFinish:  'fb4a0170907947f2addaac7462591152',
+   photoBookSize:         '982e3b5526264b69affb9a79c5ba12b2',
+   metalPrintSize:        '08c3df0d14534433aaceb466e5389e84',
+   metalPrintBackType:    'edd69c9cc9f54b80ad6537cd64a60f15',
+   metalPrintFinish:      '76910be21d814407b1cde6aa404c22dd',
+   posterPaperType:       'ff8047047dfb412da9789377136890b3',
+   posterPaperFinish:     'bf2954fad44141e5ab8410c4aab8b5b1',
+   posterSize:            '9dc5427af45843338e2393ec2c873ebf',
+   printPaperType:        '2debb07fffe64d82bbd840091a49b95c',
+   printSize:             '8f093c04ef7c4c8e8acacf40f8190543',
+   professPrintPaperType: '321baf8f8e024802a24c31e465b5f014',
+   professPrintSize:      'c2adab31c6534f7aa5b193690dd84436',
+   pillowSize:            '3deecff5a6714704b3ec117ef3196f4b',
+   pillowType:            '29b632a3cc214652ae5a3ce05ebc7cb7',
+   pillowMaterial:        'd31554b045614cf6a8e842f3d81842be',
+   tShirtPrintLocation:   '2333ee6b1aa5478b8d632ba2e9ea72de',
+   tShirtBrand:           'a8983ca8cb74406e91b3f032325d2798',
+   tShirtColor:           '09259805645b41c380e1e2e1c89579cf',
+   tShirtSize:            'e42a9daf060e43d19c7e6be1d74025db',
+   tShirtGender:          'd12e7f7478dc455aa5985162534a5b51',
+   washClothSize:         '5e297b3e8ba94cf2b849cc0d84d26485',
+   youthShirtLocation:    '8eea770225f54b26a852f87761ca11b8',
+   youthShirtBrand:       'fb4a095db470498fb2a8a2f1e9b903af',
+   youthShirtColor:       '60cc18ab2fdd4c25afa0d565965184ae',
+   youthShirtSize:        '56e4a698ea784dddb8672c6a9f7201a3',
+   youthShirtGender:      '208dce410979466ab2d8fcb04d8bc49d',
 }
 
 var sizeProperties = [
@@ -92,28 +133,75 @@ var sizeProperties = [
   optionsMap.floormatSize,
   optionsMap.foldedCardsSize,
   optionsMap.framedCanvasSize,
+  optionsMap.framedPrintSize,
+  optionsMap.gicleePrintSize,
+  optionsMap.photoBookSize,
+  optionsMap.metalPrintSize,
+  optionsMap.posterSize,
+  optionsMap.printSize,
+  optionsMap.professPrintSize,
+  optionsMap.pillowSize,
+  optionsMap.washClothSize,
 ]
-
 var thicknessProperties = [
   optionsMap.acrylicBlockThickness,
   optionsMap.acrylicPrintThickness,
   optionsMap.framedCanvasThickness,
 ]
-
 var materialOptions =  [
   optionsMap.clothNapkinMaterial,
   optionsMap.duvetCoverMaterial,
-  optionsMap.floormatMaterial
+  optionsMap.floormatMaterial,
+  optionsMap.pillowMaterial,
 ]
-
 var paperTypeOptions = [
   optionsMap.foldedCardsPaperType,
-  optionsMap.flatCardsPaperType
+  optionsMap.flatCardsPaperType,
+  optionsMap.gicleePrintPaperType,
+  optionsMap.photoBookPaperType,
+  optionsMap.posterPaperType,
+  optionsMap.printPaperType,
+  optionsMap.professPrintPaperType
 ]
-
 var packSizeOptions = [
   optionsMap.foldedCardsPackSize,
-  optionsMap.flatCardsPackSize
+  optionsMap.flatCardsPackSize,
+]
+var colorOptions = [
+  optionsMap.framedPrintColor,
+  optionsMap.framedCanvasColor,
+  optionsMap.hoodieColor,
+  optionsMap.tShirtColor,
+  optionsMap.youthShirtColor,
+]
+var typeOptions = [
+  optionsMap.coasterType,
+  optionsMap.framedPrintType,
+  optionsMap.pillowType,
+]
+var paperFinishOptions = [
+  optionsMap.posterPaperFinish,
+  optionsMap.photoBookPaperFinish,
+]
+var printLocationOptions = [
+  optionsMap.hoodiePrintLocation,
+  optionsMap.tShirtPrintLocation,
+  optionsMap.youthShirtLocation,
+]
+var genderOptions = [
+  optionsMap.hoodieGender,
+  optionsMap.tShirtGender,
+  optionsMap.youthShirtGender,
+]
+var clothingSizeOptions = [
+  optionsMap.hoodieSize,
+  optionsMap.tShirtSize,
+  optionsMap.youthShirtSize
+]
+var brandOptions = [
+  optionsMap.hoodieBrand,
+  optionsMap.tShirtBrand,
+  optionsMap.youthShirtBrand
 ]
 
 function extractAndSetOptions(options, retVal) {
@@ -126,26 +214,45 @@ function extractAndSetOptions(options, retVal) {
       var vals = extractHeightWidth(o.Value)
       retVal.height = vals.height
       retVal.width = vals.width
-    } else if (canvasWrapType === optionId) {
+    } else if (optionsMap.canvasWrapType === optionId) {
       retVal.wrapType = o.Value
     } else if (contains(materialOptions, optionId)) {
       retVal.material = o.Value
-    } else if (coasterType === optionId) {
+    } else if (contains(typeOptions, optionId)) {
       retVal.type = o.Value
-    } else if (coasterQuantity === optionId) {
+    } else if (optionsMap.coasterQuantity === optionId) {
       retVal.quantity = o.Value
-    } else if (duvetCoverBedType === optionId) {
+    } else if (optionsMap.duvetCoverBedType === optionId) {
       retVal.bedType = o.Value
-    } else if (duvetCoverBackColor === optionId) {
+    } else if (optionsMap.duvetCoverBackColor === optionId) {
       retVal.backColor = o.Value
     } else if (contains(paperTypeOptions, optionId)) {
       retVal.paperType = o.Value
     } else if (contains(packSizeOptions, optionId)) {
       retVal.packSize = o.Value
-    } else if (framedCanvasColor === optionId) {
+    } else if (contains(colorOptions, optionId)) {
       retVal.color = o.Value
+    } else if (optionsMap.framedPrintMatSize === optionId) {
+      retVal.matSize = o.Value
+    } else if (contains(clothingSizeOptions, optionId)) {
+      retVal.size = o.Value
+    } else if (contains(brandOptions, optionId)) {
+      retVal.brand = o.Value
+    } else if (contains(genderOptions, optionId)) {
+      retVal.gender = o.Value
+    } else if (contains(printLocationOptions, optionId)) {
+      retVal.printLocation = o.Value
+    } else if (optionsMap.photoBookPageCount === optionId) {
+      retVal.pageCount = o.Value
+    } else if (optionsMap.photoBookCoverType === optionId) {
+      retVal.coverType = o.Value
+    } else if (contains(paperFinishOptions, optionId)) {
+      retVal.paperFinish = o.Value
+    } else if (optionsMap.metalPrintBackType === optionId) {
+      retVal.backingType = o.Value
+    } else if (optionsMap.metalPrintFinish === optionId) {
+      retVal.finish = o.Value
     }
-
   })
   return retVal
 }
@@ -155,70 +262,49 @@ function getBaseVals(v) {
     id: newId(),
     vendorId: v.Sku,
     printingPrice: getPrintingPrice(v),
-    salePrice: getSalePrice(v)}
-}
-
-function getBaseValsWithInches(v) {
-  var retVal = getBaseVals(v)
-  retVal.units = 'inches'
-  return retVal
-}
-
-function extractFnForRectangles(v) {
-  var retVal = getBaseValsWithInches(v)
-  return extractAndSetOptions(v.Options, retVal)
-}
-
-var margin = 1.4
-var extractionMap = {
-  'Acrylic Blocks': extractFnForRectangles,
-  'Acrylic Prints': extractFnForRectangles,
-  'Art Posters': extractFnForRectangles,
-  'Canvas Minis': extractFnForRectangles,
-  'Canvas Posters': extractFnForRectangles,
-  'Canvas Wraps': extractFnForRectangles,
-  'Cloth Napkins': extractFnForRectangles,
-  'Coasters': extractFnForRectangles,
-  'Duvet Covers': extractFnForRectangles,
-  'Flat Cards': extractFnForRectangles,
-  'Floormat': extractFnForRectangles,
-  'Folded Cards': extractFnForRectangles,
-  'Framed Canvas': function(v) {
-  },
-  'Framed Prints': function(v) {
-  },
-  'Giclee Art Prints': function(v) {
-  },
-  'Hoodies': function(v) {
-  },
-  'Layflat Photobooks': function(v) {
-  },
-  'Metal Prints': function(v) {
-  },
-  'Posters': function(v) {
-  },
-  'Prints': function(v) {
-  },
-  'Professional Prints': function(v) {
-  },
-  'Throw Pillows': function(v) {
-  },
-  'T-Shirts': function(v) {
-  },
-  'Wash Cloths': function(v) {
-  },
-  'Youth Apparel': function(v) {
+    salePrice: getSalePrice(v),
+    units: 'inches'
   }
 }
 
+function setOptions(v) {
+  var retVal = getBaseVals(v)
+  return extractAndSetOptions(v.Options, retVal)
+}
+
+// - 'Acrylic Blocks'
+// - 'Acrylic Prints'
+// - 'Art Posters'
+// - 'Canvas Minis'
+// - 'Canvas Posters'
+// - 'Canvas Wraps'
+// - 'Cloth Napkins'
+// - 'Coasters'
+// - 'Duvet Covers'
+// - 'Flat Cards'
+// - 'Floormat'
+// - 'Folded Cards'
+// - 'Framed Canvas'
+// - 'Framed Prints'
+// - 'Giclee Art Prints'
+// - 'Hoodies'
+// - 'Layflat Photobooks'
+// - 'Metal Prints'
+// - 'Posters'
+// - 'Prints'
+// - 'Professional Prints'
+// - 'Throw Pillows'
+// - 'T-Shirts'
+// - 'Wash Cloths'
+// - 'Youth Apparel'
+
 service.getProducts('us','us','usd', function(d) {
-  //console.log('got d: ', d)
   async.map(
-   d.Products.filter(function(p) {
+   //d.Products.filter(function(p) {
      //return p.HasAvailableProductVariants
-     return p.Name ===  'Framed Canvas'
-   })
-   .map(function(p) {
+     //return p.Name === 'Posters'
+   //})
+   d.Products.map(function(p) {
     var images = p.Images.map(function(i) { return i.Url })
     var imageUrl = images.pop()
     return {
@@ -234,22 +320,12 @@ service.getProducts('us','us','usd', function(d) {
   function(product, cb) {
     console.log('getting variants for product: ', product.name)
     service.getProductVariants('us', product.vendorId, function(variants) {
-      // Here transform to the proper form.
-      // And set them on product
-      var fn = extractionMap[product.name]
-
-      variants.ProductVariants.forEach(function(v) {
-        v.Options.forEach(function(o) {
-          console.log('option name, id: ', o.Name, ', ', o.OptionId)
-          console.log('value: ', o.Value)
-        })
-      })
-      //variants = variants.ProductVariants.map(fn)
-      //console.log('variants: ', variants)
-      cb(null, variants)
+      var newVariants = variants.ProductVariants.map(setOptions)
+      console.log('new variants: ', newVariants)
+      cb(null, newVariants)
     })
   },
   function(err, results) {
-    //console.log("got variants: ", results)
+    console.log("got variants: ", results)
   })
 })
