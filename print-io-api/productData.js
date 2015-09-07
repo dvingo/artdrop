38318cb75613bec45fee165a57118d525bca7dc4
Dan Vingo <danvingo@gmail.com>
2015-09-07 16:09:25 -0400

    Move product data into its own file

---
+++ print-io-api/productData.js
@@ -0,0 +1,158 @@
+var optionsMap = {
+   acrylicBlockSize:      '226c9e02a1ce4e37bcc17ab6e229ba90',
+   acrylicBlockThickness: '96b7de2821a64b47b6e7c172d3cc4e77',
+   acrylicPrintSize:      '67a673fd52224aa6866aba1485508422',
+   acrylicPrintThickness: '5927d683883e4b63923468d524f15a61',
+   artPosterSize:         'eb0305d195c949d987ec86c4c6d57f2c',
+   canvasMiniSize:        '9f9c9d1c4b704356b55a04904971a4c0',
+   canvasPosterSize:      '315c04b70e67494fbb203b0ef023fe0b',
+   canvasWrapSize:        '568b5efc04d54cfcbba692350bba6248',
+   canvasWrapType:        '53690280519549a3bc7ed05c92607f11',
+   clothNapkinSize:       '16f6b99d55b348c6bd6980fb59e929db',
+   clothNapkinMaterial:   'b18b0f299aa74be192faae081d20ab8a',
+   coasterSize:           'ff2e86a5f0fd4191b336b5acf7bff37a',
+   coasterType:           '71050dfa2eff461fbc195a549e6eb3d0',
+   coasterQuantity:       '13f0a64b7b3c449280141dbd8f17b2b8',
+   duvetCoverBackColor:   'a7fc54c639384c49a255b1611eead7d3',
+   duvetCoverMaterial:    '977402ea93bb4db781086e8be9ebf943',
+   duvetCoverBedType:     '2ccec03491754603a51d1fa00c5649fb',
+   duvetCoverSize:        '02b03365db27421b9ac451c0e5ea0638',
+   flatCardsSize:         '439f4e4bb6fd412aaf4cbed2e83d50da',
+   flatCardsPaperType:    '0a8204aec5b24b4cbaff249fc2106943',
+   flatCardsPackSize:     '88eb54eb707942918cb5e61dc0ccc2de',
+   floormatSize:          '504d3db60f2d4d29913ae9bcbad860c2',
+   floormatMaterial:      '360e092fdef94b5e94ef3b417933b66e',
+   foldedCardsSize:       'da46edb521d14c13b5cca8babbd31d34',
+   foldedCardsPaperType:  '62c313f2ddbc4a7cb2a36b7bc9e7c59d',
+   foldedCardsPackSize:   '283510fe0bea4d4ea82d378ed38bb1d7',
+   foldedCardsSideType:   '0b53534cb0984ed8b0bd991caefb4dc8',
+   framedCanvasSize:      '26b66c145877439ab7eb765ad5a52309',
+   framedCanvasColor:     '7702d54940094ce5bbb625a816e6a56d',
+   framedCanvasThickness: '7e0232ca5e604a18bbb35d192952f1a6',
+   framedPrintMatSize:    '0d408d36ab9d4da5aa7d338f3bf0a6f1',
+   framedPrintColor:      'af66458cf1b844ad9daed9546a6e2216',
+   framedPrintSize:       '3e9da285a0a84ea59633207e0feff5fe',
+   framedPrintType:       'c02deea045fb4d47873985b333db9321',
+   gicleePrintPaperType:  '0d14875084904097a8d5f69133081692',
+   gicleePrintSize:       '2745aa0a75694c599809ea402185954d',
+   hoodieColor:           '50326f0998634f78b51c5e7f68f2dde3',
+   hoodieSize:            'd0ad1ca109d248e09e24f9347f1d4ac8',
+   hoodieBrand:           'c366f11045444bdc93dcf55d5ccc5a3d',
+   hoodieGender:          '9c6a6a75bfb749e6b1fb7e40d76f8948',
+   hoodiePrintLocation:   '3f7e8f484fd946aeb8a6af4cc0e54e7a',
+   photoBookPageCount:    'bf1f9d742519451191e1ef2698727869',
+   photoBookCoverType:    'c42898b2bc5b41728af0c85f5a99dacc',
+   photoBookPaperType:    '3763b6a1e30540e99b466d844b32d381',
+   photoBookPaperFinish:  'fb4a0170907947f2addaac7462591152',
+   photoBookSize:         '982e3b5526264b69affb9a79c5ba12b2',
+   metalPrintSize:        '08c3df0d14534433aaceb466e5389e84',
+   metalPrintBackType:    'edd69c9cc9f54b80ad6537cd64a60f15',
+   metalPrintFinish:      '76910be21d814407b1cde6aa404c22dd',
+   posterPaperType:       'ff8047047dfb412da9789377136890b3',
+   posterPaperFinish:     'bf2954fad44141e5ab8410c4aab8b5b1',
+   posterSize:            '9dc5427af45843338e2393ec2c873ebf',
+   printPaperType:        '2debb07fffe64d82bbd840091a49b95c',
+   printSize:             '8f093c04ef7c4c8e8acacf40f8190543',
+   professPrintPaperType: '321baf8f8e024802a24c31e465b5f014',
+   professPrintSize:      'c2adab31c6534f7aa5b193690dd84436',
+   pillowSize:            '3deecff5a6714704b3ec117ef3196f4b',
+   pillowType:            '29b632a3cc214652ae5a3ce05ebc7cb7',
+   pillowMaterial:        'd31554b045614cf6a8e842f3d81842be',
+   tShirtPrintLocation:   '2333ee6b1aa5478b8d632ba2e9ea72de',
+   tShirtBrand:           'a8983ca8cb74406e91b3f032325d2798',
+   tShirtColor:           '09259805645b41c380e1e2e1c89579cf',
+   tShirtSize:            'e42a9daf060e43d19c7e6be1d74025db',
+   tShirtGender:          'd12e7f7478dc455aa5985162534a5b51',
+   washClothSize:         '5e297b3e8ba94cf2b849cc0d84d26485',
+   youthShirtLocation:    '8eea770225f54b26a852f87761ca11b8',
+   youthShirtBrand:       'fb4a095db470498fb2a8a2f1e9b903af',
+   youthShirtColor:       '60cc18ab2fdd4c25afa0d565965184ae',
+   youthShirtSize:        '56e4a698ea784dddb8672c6a9f7201a3',
+   youthShirtGender:      '208dce410979466ab2d8fcb04d8bc49d',
+}
+exports.optionsMap = optionsMap
+
+exports.sizeProperties = [
+  optionsMap.acrylicBlockSize,
+  optionsMap.acrylicPrintSize,
+  optionsMap.artPosterSize,
+  optionsMap.canvasMiniSize,
+  optionsMap.canvasPosterSize,
+  optionsMap.canvasWrapSize,
+  optionsMap.clothNapkinSize,
+  optionsMap.coasterSize,
+  optionsMap.duvetCoverSize,
+  optionsMap.flatCardsSize,
+  optionsMap.floormatSize,
+  optionsMap.foldedCardsSize,
+  optionsMap.framedCanvasSize,
+  optionsMap.framedPrintSize,
+  optionsMap.gicleePrintSize,
+  optionsMap.photoBookSize,
+  optionsMap.metalPrintSize,
+  optionsMap.posterSize,
+  optionsMap.printSize,
+  optionsMap.professPrintSize,
+  optionsMap.pillowSize,
+  optionsMap.washClothSize,
+]
+exports.thicknessProperties = [
+  optionsMap.acrylicBlockThickness,
+  optionsMap.acrylicPrintThickness,
+  optionsMap.framedCanvasThickness,
+]
+exports.materialOptions =  [
+  optionsMap.clothNapkinMaterial,
+  optionsMap.duvetCoverMaterial,
+  optionsMap.floormatMaterial,
+  optionsMap.pillowMaterial,
+]
+exports.paperTypeOptions = [
+  optionsMap.foldedCardsPaperType,
+  optionsMap.flatCardsPaperType,
+  optionsMap.gicleePrintPaperType,
+  optionsMap.photoBookPaperType,
+  optionsMap.posterPaperType,
+  optionsMap.printPaperType,
+  optionsMap.professPrintPaperType
+]
+exports.packSizeOptions = [
+  optionsMap.foldedCardsPackSize,
+  optionsMap.flatCardsPackSize,
+]
+exports.colorOptions = [
+  optionsMap.framedPrintColor,
+  optionsMap.framedCanvasColor,
+  optionsMap.hoodieColor,
+  optionsMap.tShirtColor,
+  optionsMap.youthShirtColor,
+]
+exports.typeOptions = [
+  optionsMap.coasterType,
+  optionsMap.framedPrintType,
+  optionsMap.pillowType,
+]
+exports.paperFinishOptions = [
+  optionsMap.posterPaperFinish,
+  optionsMap.photoBookPaperFinish,
+]
+exports.printLocationOptions = [
+  optionsMap.hoodiePrintLocation,
+  optionsMap.tShirtPrintLocation,
+  optionsMap.youthShirtLocation,
+]
+exports.genderOptions = [
+  optionsMap.hoodieGender,
+  optionsMap.tShirtGender,
+  optionsMap.youthShirtGender,
+]
+exports.clothingSizeOptions = [
+  optionsMap.hoodieSize,
+  optionsMap.tShirtSize,
+  optionsMap.youthShirtSize
+]
+exports.brandOptions = [
+  optionsMap.hoodieBrand,
+  optionsMap.tShirtBrand,
+  optionsMap.youthShirtBrand
+]
